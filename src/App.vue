bf284229f6769a9dc25a0435c837fa9c109cf4e4
Optiq01 <Optiqvision@gmail.com>
2023-06-12 19:36:52 -0500

    detect when plus button animation ends

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,17 @@
 <script setup lang="ts">
+  import {ref} from 'vue';
+  import type { Ref } from 'vue';
+  import {Motion} from 'motion/vue';
+
+  const plusButton = document.getElementById('plusSign');
+  let plusButtonComplete: Ref<boolean> = ref(false);
+  
+  plusButton?.addEventListener('animationend', ()=>{
+   plusButtonComplete = ref(true);
+  });
+
+
+
 </script>
 
 <template>
@@ -7,8 +20,8 @@
     <article class="titleArea z-20 w-full h-full">
       <h1 class="title align-middle sm:text-[length:var(--text-ultraJumbotron-2)] md:text-[length:var(--text-ultraJumbotron-3)] text-white">Explore</h1>
       <div class="plusBtnContainer gap-[var(--micro-1-3)]">
-        <img src="./assets/Plus.svg" class="sm:w-[var(--xTiny-1-3)]" />
-        <p class="align-center sm:text-[length:var(--text-widget-3)] md:text-[length:var(--text-article-3)]">more details</p>
+        <img id="plusSign" src="./assets/Plus.svg" class="sm:w-[var(--xTiny-1-3)]" />
+        <p class="text-white align-center sm:text-[length:var(--text-widget-3)] md:text-[length:var(--text-article-3)]">more details</p>
       </div>
     </article>
   </section>
